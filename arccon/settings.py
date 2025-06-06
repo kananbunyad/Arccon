0720d28671d32e0b32cb382b88718098f7da0170
Kanan Bunyadov <kenanbunyad@gmail.com>
2025-06-06 13:31:18 +0400

    update

--- arccon/settings.py
+++ arccon/settings.py
@@ -146,8 +146,9 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 import django_heroku
 django_heroku.settings(locals())
 
-DATABASES['default'] = dj_database_url.config(
-    default=f"sqlite:///{BASE_DIR}/db.sqlite3",  # SQLite varsayılan bağlantısı
-    conn_max_age=600,
-    ssl_require=True
-)
+DATABASES = {
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600
+    )
+}
